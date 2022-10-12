56ce39756a29c57b04990ea6ab7681329db3b823
FVSilva <felupesilva1@gmail.com>
2022-10-12 14:49:44 -0300

    Crud simples para criação de pessoas no banco de dados usando os metodos GET/POST/PATCH/DELETE

---
+++ API's/API_CRUD_CAD_PESSOAS/routes/personalRoutes.js
@@ -0,0 +1,126 @@
+const Router = require('express').Router();
+
+const { response } = require('express');
+const Person = require('../models/Person');
+
+//Create -criação de dados
+Router.post('/', async (req, res) => {
+
+    // req.body
+    const {name, salary, approved} = req.body
+
+    if(!name) {
+        res.status(422).json({error : 'O nome é obrigatorio'})
+        return
+    }
+
+    const person  = {
+        name,
+        salary,
+        approved
+    }
+     try {        
+        //criando dados
+        await Person.create(person)
+
+        //criado com sucesso
+        res.status(201).json({  message: 'Pessoa inserida no sistema com sucesso!' })
+
+     } catch (error) {
+        res.status(500).json({error: error})
+     }
+
+})
+
+//Read = leitura de dados
+Router.get('/', async (req, res) => {
+    try {
+        
+        const people =  await Person.find();
+
+        res.status (200).json(people)
+
+    } catch (error) {
+        res.status(500).json({  error: error})
+    }
+})
+
+
+Router.get('/:id', async (req, res) => {
+    
+    //extrair o dado da requisição pela url = rq.params
+    const id = req.params.id
+
+    try {
+        
+        const person = await Person.findOne({_id: id})
+
+        if(!person) {
+            res.status(422).json({ message: 'O usuario não foi encontrado '})
+            return
+        }
+
+        res.status(200).json(person)
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+    //Update - atualização de dados (PUT, PATCH)
+    Router.patch('/:id', async(req, res) => {
+
+        const id = req.params.id
+
+        const { name, salary, approved } = req.body
+
+        const person = {
+            name,
+            salary,
+            approved,
+        }
+        try {
+            
+            const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+            console.log(updatedPerson)
+
+            if(updatedPerson.matchedCount === 0) {
+                res.status(422).json({ message: 'O usuario não foi encontrado '})
+                return
+            }
+
+            res.status(200).json(person)
+
+        } catch (error) {
+            res.status(500).json({ error: error })
+        }
+
+    })
+
+    //Delete - deletar dados
+    Router.delete('/:id', async (req, res) => {
+
+        const id = req.params.id
+
+        const person = await Person.findOne({ _id: id })
+
+        if(!person) {
+            res.status(422).json({ message: 'O usuario não foi encontrado '})
+            return
+        }
+
+        try {
+            
+            await Person.deleteOne({ _id: id })
+
+            res.status(200).json({ message: 'Usuario removido com sucesso!' })
+
+        } catch (error) {
+            res.status(500).json({ error: error })
+        }
+
+    })
+
+})
+
+module.exports = Router; 
